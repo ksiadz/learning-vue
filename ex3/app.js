2ab34df425321baac18e20d83b5b84313bf36e96
Wojciech Kokorzycki <wojciech@clusterone.com>
2018-06-10 03:44:11 +0200

    Added watcher and computer type, setimeout used in watch to do task in specific schedule

---
+++ ex3/app.js
@@ -0,0 +1,19 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: 0
+        },
+        computed: {
+            result: function () {
+                return this.value == 37? "done" : "not there";
+            }
+        },
+        watch: {
+            value: function (value) {
+                var vm = this;
+                setTimeout(function () {
+                    vm.value = 0;
+                }, 1000);
+            }
+        }
+    });
